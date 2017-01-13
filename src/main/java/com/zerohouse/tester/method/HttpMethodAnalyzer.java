9ef7044f50af0f1582f4bfbda0c386de4e7477f6
park <parksungho86@gmail.com>
2017-01-13 17:35:33 +0900

    deb

--- src/main/java/com/zerohouse/tester/method/HttpMethodAnalyzer.java
+++ src/main/java/com/zerohouse/tester/method/HttpMethodAnalyzer.java
@@ -10,9 +10,7 @@ public class HttpMethodAnalyzer implements MethodAnalyzer {
     @Override
     public void analyze(Method method, Map apiAnalysis) {
         RequestMapping methodAnnotation = method.getAnnotation(RequestMapping.class);
-        RequestMethod httpMethod = RequestMethod.GET;
         if (methodAnnotation.method().length != 0)
-            httpMethod = methodAnnotation.method()[0];
-        apiAnalysis.put("method", httpMethod);
+            apiAnalysis.put("method", methodAnnotation.method()[0]);
     }
 }
