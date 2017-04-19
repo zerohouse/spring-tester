257f9f413393e88a5f1b34da415e1dbaabbd678c
Park <parksungho86@gmail.com>
2017-04-20 02:05:47 +0900

    5.2

--- src/test/com/zerohouse/tester/SpringApiAnalyzerTest.java
+++ src/test/com/zerohouse/tester/SpringApiAnalyzerTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class SpringApiAnalyzerTest {
     @Test
     public void generateTestPage() throws Exception {
-        SpringApiTester springApiTester = new SpringApiTester("com.zerohouse");
+        SpringApiTester springApiTester = new SpringApiTester("com.zerohouse", null);
         springApiTester.setTitle("편돌이");
         springApiTester.putHttpHeader("asdf", "asdf");
         springApiTester.generateTestPageHtml("test.html");
