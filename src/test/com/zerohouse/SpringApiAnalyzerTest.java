51cbf8613787af72dd31f4a66a58273438503266
park <parksungho86@gmail.com>
2018-05-18 12:01:21 +0900

    0.8.4
    -> GetMapping, PostMapping, DeleteMapping, PutMapping 지원
    -> CustomPageUrl 설정
    -> 설정 방법 바뀜 (Bean등록)

--- src/test/com/zerohouse/SpringApiAnalyzerTest.java
+++ src/test/com/zerohouse/SpringApiAnalyzerTest.java
@@ -6,9 +6,6 @@ import org.junit.Test;
 public class SpringApiAnalyzerTest {
     @Test
     public void generateTestPage() throws Exception {
-        SpringApiTester springApiTester = new SpringApiTester("com.zerohouse", "페이지 제목", "/api/testPage");
-        springApiTester.putHttpHeader("asdf", "asdf");
-        springApiTester.generateTestPageHtml("test.html");
     }
 
 }
